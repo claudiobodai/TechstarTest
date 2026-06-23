edd961f6aef271ac780d5e166d223f7c14b87897
claudiobodai <bodaiclaudio@icloud.com>
2026-06-23 10:30:04 +0200

    Add: CQRS product creation and refactor validation formatting

--- TechstarTest/Behaviors/ValidationBehavior.cs
+++ TechstarTest/Behaviors/ValidationBehavior.cs
@@ -18,8 +18,14 @@ public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TReques
         if (_validators.Any())
         {
             var context = new ValidationContext<TRequest>(request);
-            var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
-            var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+            
+            var validationResults = await Task.WhenAll
+                (_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+            
+            var failures = validationResults
+                .SelectMany(r => r.Errors)
+                .Where(f => f != null)
+                .ToList();
 
             if (failures.Count != 0)
             {
---
+++ TechstarTest/Features/Products/CreateProduct.cs
@@ -0,0 +1,61 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace TechstarTest.Features.Products;
+
+public record CreateProductCommand(string Name, decimal Price) : IRequest<int>;
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Il Nome è obbligatorio.")
+            .MaximumLength(100);
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("Il prezzo deve essere maggiore di zero.");
+    }
+}
+
+public record ProductCreatedEvent(int ProductId, string Name) : INotification;
+
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, int>
+{
+   private readonly IMediator mediator;
+
+    // Db context 
+
+    public CreateProductCommandHandler(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    public async Task<int> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+       
+        int newProductId = new Random().Next(1, 1000); 
+
+        await mediator.Publish(new ProductCreatedEvent(newProductId, request.Name), cancellationToken);
+       
+        return newProductId;
+    }
+}
+
+public class ProductCreatedEventHandler : INotificationHandler<ProductCreatedEvent>
+{
+    private readonly ILogger<ProductCreatedEventHandler> _logger;
+
+    public ProductCreatedEventHandler(ILogger<ProductCreatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task Handle(ProductCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Evento di Dominio: Creato con successo il prodotto '{ProductName}' con ID {ProductId}",
+            notification.Name, notification.ProductId);
+
+        await Task.CompletedTask;
+    }
+}
